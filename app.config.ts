60e676ffd26e7679e1e0f882a3126b62f2f6e2e9
DAngelo <dangelo.watson1212@gmail.com>
2025-12-30 02:23:49 -0500

    -

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "AI Icons",
     slug: "ai-icon-generator",
-    version: "1.0.25",
+    version: "1.0.26",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
